0db4df134b03d6b60531eea8c98cda0341197dd1
Morningwood420 <mourat723@gmail.com>
2022-09-24 20:59:53 +0300

    Initial Commit

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Giannis Mouratidis on 24/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
